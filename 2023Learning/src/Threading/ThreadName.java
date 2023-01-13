df7e446f7a95db0732269b247959ca6501d53a21
sonali <sonali@SONALI>
2023-01-13 23:23:45 +0530

    first commit

---
+++ 2023Learning/src/Threading/ThreadName.java
@@ -0,0 +1,10 @@
+package Threading;
+
+public class ThreadName {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Thread.currentThread().getName());
+	}
+
+}
